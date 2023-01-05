deea2683cd50877c56f2168ae10457183f620375
Takuto Yanagida <yanagida@stxst.com>
2023-01-05 23:57:44 +0900

    Rename old masonry as fence and add real masonry

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,19 +2,22 @@
  * Gulpfile
  *
  * @author Takuto Yanagida
- * @version 2022-12-13
+ * @version 2023-01-05
  */
 
 const WATCH_OPTS = { ignoreInitial: false, delay: 400 };
 
 import gulp from 'gulp';
 
+import { makeJsTask } from './gulp/task-js.mjs';
 import { makeSassTask } from './gulp/task-sass.mjs';
 
+const js   = makeJsTask('src/js/[^_]*.js', './dist/js', './src/js');
 const sass = makeSassTask('src/sass/[^_]*.scss', './dist/css', './src/sass');
 
-export const build = gulp.parallel(sass);
+export const build = gulp.parallel(js, sass);
 export default () => {
+	gulp.watch('src/**/*.js', WATCH_OPTS, js);
 	gulp.watch('src/**/*.scss', WATCH_OPTS, sass);
 };
 
@@ -26,10 +29,12 @@ export const doc = async () => {
 	const { makeCopyTask }      = await import('./gulp/task-copy.mjs');
 	const { makeTimestampTask } = await import('./gulp/task-timestamp.mjs');
 
+	const doc_js        = makeCopyTask('dist/js/*', './docs/js');
 	const doc_css       = makeCopyTask('dist/css/*', './docs/css');
 	const doc_sass      = makeSassTask('docs/style.scss', './docs/css');
 	const doc_timestamp = makeTimestampTask('docs/**/*.html', './docs');
 
+	gulp.watch('src/**/*.js', WATCH_OPTS, gulp.series(js, doc_js, doc_timestamp));
 	gulp.watch('src/**/*.scss', WATCH_OPTS, gulp.series(sass, doc_css, doc_timestamp));
 	gulp.watch('docs/style.scss', WATCH_OPTS, gulp.series(doc_sass, doc_timestamp));
 };
